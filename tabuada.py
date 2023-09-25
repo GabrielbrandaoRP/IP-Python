4f05ef1c1cc5716dc5e42dbd3fd56969a6833e84
GabrielRPB <gabrielhand15@gmail.com>
2023-09-25 19:23:04 -0300

    Exemplo de tabuada em Python mostrada em sala de aula.

---
+++ tabuada.py
@@ -0,0 +1,8 @@
+#aluno: Gabriel Roberto Pinheiro Brandão
+#matricula 2023211510032
+#Faça um programa que mostre as
+# tabuadas dos números de 1 a 10.
+
+for i in range(1, 11):
+        for j in range(1, 11):
+                print(f'{i} x {j} = {(i*j)}')
