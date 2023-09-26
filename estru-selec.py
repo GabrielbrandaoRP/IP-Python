1d3eb53ce3ce41c0b71d28ea8012abf8d1435d7f
GabrielRPB <gabrielhand15@gmail.com>
2023-09-25 21:17:13 -0300

    Novas listas

---
+++ estru-selec.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite a sua idade: "))
+
+if idade > 17:
+    print("Pode entrar na festa!")
+elif idade > 16 and idade < 18:
+    print("É hora de voltar para sua casa")
+else:
+    print("Vou chamar seus pais.")
---
+++ exercicio3.py
@@ -0,0 +1,4 @@
+import random var = random.randint([0, 100])
+    print()
+
+
